bbbf03ebb769af6405cdb520c458592fbc435b80
Andre Wells <awells@bscglobal.com>
2023-01-08 09:37:56 +0200

    WIP setting up linting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+function App() 
+{
   const [count, setCount] = useState(0)
 
   return (
